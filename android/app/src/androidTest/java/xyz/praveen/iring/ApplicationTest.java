1e69d979c865817332818fa3ce55c2ab5ec60e6d
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-06-15 21:51:48 +0200

    Android project init

---
+++ android/app/src/androidTest/java/xyz/praveen/iring/ApplicationTest.java
@@ -0,0 +1,13 @@
+package xyz.praveen.iring;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
