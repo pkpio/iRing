404b7f4036f631ff07c041073e1587a9fc395d6c
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-07-09 18:40:39 +0200

    Sample UI for testing implemented

--- android/app/src/main/java/xyz/praveen/iring/MainActivity.java
+++ android/app/src/main/java/xyz/praveen/iring/MainActivity.java
@@ -1,32 +1,32 @@
 package xyz.praveen.iring;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
+
+import com.astuetz.PagerSlidingTabStrip;
 
 import xyz.praveen.iring.accesscontrol.AccessController;
 import xyz.praveen.iring.server.OnGadgetActionListener;
 import xyz.praveen.iring.server.ServerHandle;
 import xyz.praveen.iring.touchpattern.OnTouchListener;
-import xyz.praveen.iring.touchpattern.TouchHandler;
+
+import static xyz.praveen.iring.util.LogUtils.makeLogTag;
 
 
 public class MainActivity extends AppCompatActivity implements OnGadgetActionListener,
         OnTouchListener {
-    final String DEBUG_TAG = this.getClass().getName();
+    final static String TAG = makeLogTag(MainActivity.class);
 
     AccessController mAccessControl;
-    TextView helloView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        helloView = (TextView) findViewById(R.id.hello);
-        helloView.setOnTouchListener(new TouchHandler(this, this));
 
         // Admin permission check
         mAccessControl = new AccessController();
@@ -35,6 +35,14 @@ public class MainActivity extends AppCompatActivity implements OnGadgetActionLis
         // Init server
         ServerHandle mServerHandle = new ServerHandle(this);
         mServerHandle.startServer();
+
+        // Initialize the ViewPager and set an adapter
+        ViewPager pager = (ViewPager) findViewById(R.id.content_pager);
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
+        // Bind the tabs to the ViewPager
+        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.sliding_tabs);
+        tabs.setViewPager(pager);
     }
 
     @Override
@@ -42,13 +50,42 @@ public class MainActivity extends AppCompatActivity implements OnGadgetActionLis
         this.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                helloView.setText("Action was : " + action);
+                // -TODO- Action
             }
         });
     }
 
     @Override
     public void onTouchEvent(int event) {
+
+    }
+
+    /**
+     *
+     */
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        private final String[] TITLES = {"Categories", "Home", "Top Paid", "Top Free", "Top Grossing", "Top New Paid",
+                "Top New Free", "Trending"};
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return SampleFragment.newInstance(position);
+        }
 
     }
 }
---
+++ android/app/src/main/java/xyz/praveen/iring/SampleFragment.java
@@ -0,0 +1,43 @@
+package xyz.praveen.iring;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+/**
+ * Created by praveen on 9/7/15.
+ */
+public class SampleFragment extends Fragment {
+
+    private static final String ARG_POSITION = "position";
+
+    private int position;
+
+    public static SampleFragment newInstance(int position) {
+        SampleFragment f = new SampleFragment();
+        Bundle b = new Bundle();
+        b.putInt(ARG_POSITION, position);
+        f.setArguments(b);
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        position = getArguments().getInt(ARG_POSITION);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_tab, container,
+                false);
+        ListView list = (ListView) rootView.findViewById(R.id.list);
+        list.setAdapter(new SampleListAdapter(getActivity()));
+        return rootView;
+    }
+}
